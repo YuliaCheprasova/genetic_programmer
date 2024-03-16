eb2fa871f943d2f83dcdf9567915e1fd180b884e
Yulia <cheprasovaja@yandex.ru>
2024-03-16 12:45:25 +0700

    AddNode with switch_init
    I am going to delete switch_init

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ public:
     int operation;
     int variable;
     double constant;
-    struct Node* left;
-    struct Node* mid;
-    struct Node* right;
+    Node* left;
+    Node* mid;
+    Node* right;
 
     Node(char type, int number)
     {
@@ -53,36 +53,45 @@ public:
         this->root = NULL;
     }
     // без прототипа не смогла
-    int AddNode(int type, int value, int unar_oper, int bin_oper, int depth, int* flag, int d)
+    int AddNode(int type, int value, int unar_oper, int bin_oper, int depth, int* flag, int d, int  switch_init)
     {
-        AddNode(type, value, unar_oper, bin_oper, depth, flag, d, root);
+        AddNode(type, value, unar_oper, bin_oper, depth, flag, d, switch_init, root);
         if (*flag == 0)
             return 0;
         if (*flag == 1)
             return 1;
     }
-    int AddNode(int type, double value, int unar_oper, int bin_oper, int depth, int* flag, int d)
+    int AddNode(int type, double value, int unar_oper, int bin_oper, int depth, int* flag, int d, int switch_init)
     {
-        AddNode(type, value, unar_oper, bin_oper, depth, flag, d, root);
+        AddNode(type, value, unar_oper, bin_oper, depth, flag, d, switch_init, root);
         if (*flag == 0)
             return 0;
         if (*flag == 1)
             return 1;
     }
-    void AddNode(int type, int value, int unar_oper, int bin_oper, int depth, int* flag, int d, Node*& cur_el)
+    void AddNode(int type, int value, int unar_oper, int bin_oper, int depth, int* flag, int d, int switch_init, Node*& cur_el)
     {
         // условия вставки в дерево, чтобы в листьях оказались только переменные или константы
         if(cur_el == NULL)
         {
-            if(d<depth)
+             if(d<depth)
             {
-                if (type=='o')
+                if(switch_init==0)
+                {
+                    if (type=='o')
+                    {
+                        cur_el = new Node(type, value);
+                        *flag = 1;
+                        return;
+                    }
+                    else if(type=='v'||type=='c'){return;}
+                }
+                if(switch_init==1)
                 {
                     cur_el = new Node(type, value);
                     *flag = 1;
                     return;
                 }
-                else if(type=='v'||type=='c'){return;}
             }
             else if(d==depth)
             {
@@ -99,40 +108,49 @@ public:
         // условия, в зависимости от которых определяется сколько дочерних узлов может быть у узла
         else if(cur_el->operation < unar_oper)
         {
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->left);
+            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->left);
             if(*flag==1){return;}
         }
         else if(cur_el->operation >= unar_oper && cur_el->operation < unar_oper+bin_oper)
         {
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->left);
+            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->left);
             if(*flag==1){return;}
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->right);
+            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->right);
             if(*flag==1){return;}
         }
         else if(cur_el->operation >= unar_oper+bin_oper)
         {
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->left);
+            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->left);
             if(*flag==1){return;}
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->right);
+            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->right);
             if(*flag==1){return;}
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->mid);
+            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->mid);
             if(*flag==1){return;}
         }
     }
-    void AddNode(int type, double value, int unar_oper, int bin_oper, int depth, int* flag, int d, Node*& cur_el)
+    void AddNode(int type, double value, int unar_oper, int bin_oper, int depth, int* flag, int d, int switch_init, Node*& cur_el)
     {
         // условия вставки в дерево, чтобы в листьях оказались только переменные или константы
         if(cur_el == NULL)
         {
             if(d<depth)
             {
-                if (type=='o')
+                if(switch_init==0)
+                {
+                    if (type=='o')
+                    {
+                        cur_el = new Node(type, value);
+                        *flag = 1;
+                        return;
+                    }
+                    else if(type=='v'||type=='c'){return;}
+                }
+                if(switch_init==1)
                 {
                     cur_el = new Node(type, value);
                     *flag = 1;
                     return;
                 }
-                else if(type=='v'||type=='c'){return;}
             }
             else if(d==depth)
             {
@@ -147,28 +165,89 @@ public:
             else if(d>depth){return;}
         }
         // условия, в зависимости от которых определяется сколько дочерних узлов может быть у узла
-        else if(cur_el->operation < unar_oper)
+        else if(cur_el->type=='o')
         {
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->left);
-            if(*flag==1){return;}
+            if(cur_el->operation < unar_oper)
+            {
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->left);
+                if(*flag==1){return;}
+            }
+            else if(cur_el->operation >= unar_oper && cur_el->operation < unar_oper+bin_oper)
+            {
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->left);
+                if(*flag==1){return;}
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->right);
+                if(*flag==1){return;}
+            }
+            else if(cur_el->operation >= unar_oper+bin_oper)
+            {
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->left);
+                if(*flag==1){return;}
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->right);
+                if(*flag==1){return;}
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, switch_init, cur_el->mid);
+                if(*flag==1){return;}
+            }
         }
-        else if(cur_el->operation >= unar_oper && cur_el->operation < unar_oper+bin_oper)
-        {
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->left);
-            if(*flag==1){return;}
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->right);
-            if(*flag==1){return;}
-        }
-        else if(cur_el->operation >= unar_oper+bin_oper)
-        {
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->left);
-            if(*flag==1){return;}
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->right);
-            if(*flag==1){return;}
-            AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->mid);
-            if(*flag==1){return;}
-        }
+        else if(cur_el->type=='v'||cur_el->type=='c'){return;}
     }
+
+
+
+    /*void AddNode(int type, int value, int unar_oper, int bin_oper, int depth, int* flag, int d, Node*& cur_el)
+    {
+        // условия вставки в дерево, чтобы в листьях оказались только переменные или константы
+        if(cur_el == NULL)
+        {
+            if(d<depth)
+            {
+                cur_el = new Node(type, value);
+                *flag = 1;
+                return;
+            }
+            else if(d==depth)
+            {
+                if (type=='v'||type=='c')
+                {
+                    cur_el = new Node(type, value);
+                    *flag = 1;
+                    return;
+                }
+                else if(type=='o'){return;}
+            }
+            else if(d>depth){return;}
+        }
+        // условия, в зависимости от которых определяется сколько дочерних узлов может быть у узла
+        else if(cur_el->type=='o')
+        {
+            if(cur_el->operation < unar_oper)
+            {
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->left);
+                if(*flag==1){return;}
+            }
+            else if(cur_el->operation >= unar_oper && cur_el->operation < unar_oper+bin_oper)
+            {
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->left);
+                if(*flag==1){return;}
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->right);
+                if(*flag==1){return;}
+            }
+            else if(cur_el->operation >= unar_oper+bin_oper)
+            {
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->left);
+                if(*flag==1){return;}
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->right);
+                if(*flag==1){return;}
+                AddNode(type, value, unar_oper, bin_oper, depth, flag, d+1, cur_el->mid);
+                if(*flag==1){return;}
+            }
+        }
+        else if(cur_el->type=='v'||cur_el->type=='c'){return;}
+    }*/
+
+
+
+
     void PrintTree()
     {
         PrintTree(root);
@@ -179,13 +258,27 @@ public:
         PrintTree(cur_el->left);
 
         if(cur_el->type=='o')
-            cout << cur_el->operation << endl;
+            cout << cur_el->type<< " " << cur_el->operation << endl;
         else if(cur_el->type=='v')
-            cout << cur_el->variable << endl;
+            cout << cur_el->type<< " " << cur_el->variable << endl;
         else if(cur_el->type=='c')
-            cout << cur_el->constant << endl;
+            cout << cur_el->type<< " " << cur_el->constant << endl;
         PrintTree(cur_el->mid);
         PrintTree(cur_el->right);
+    }
+
+    ~Tree()
+    {
+        ClearTree(root);
+    }
+
+    void ClearTree(Node* node) {
+        if (node != nullptr) {
+            ClearTree(node->left);
+            ClearTree(node->mid);
+            ClearTree(node->right);
+            delete node;
+        }
     }
 };
 
@@ -199,9 +292,25 @@ void synthetic_data(double* x, double* y, int num_obs)
     }
 }
 
+/*void count_f()
+{
+    double f_temp;
+    if(cur_el==NULL){return;}
+    PrintTree(cur_el->left);
+
+    if(cur_el->type=='o')
+
+    else if(cur_el->type=='v')
+        cout << cur_el->type<< " " << cur_el->variable << endl;
+    else if(cur_el->type=='c')
+        cout << cur_el->type<< " " << cur_el->constant << endl;
+    PrintTree(cur_el->mid);
+    PrintTree(cur_el->right);
+}*/
+
 int main()
 {
-    int i, j, num_obs = 400, unar_oper = 3, bin_oper = 5, trin_oper = 1, num_var = 1, num_types = 3, depth = 4, flag=0, success;
+    int i, j, num_obs = 400, unar_oper = 3, bin_oper = 5, trin_oper = 1, num_var = 1, num_types = 3, depth = 3, flag=0, success;
     // 0sin, 1cos, 2exp, 3log, 4+, 5-, 6*, 7/, 8if..then..else
     // 0x,
     // flag нужен для того, чтобы понимать добавился ли элемент в дерево, d контролирует глубину строящегося дерева
@@ -209,7 +318,7 @@ int main()
     double random_const;
     //n - количество индивидов в поколении, num_generals - количество поколений
     int switch_init = 0;
-    //0 - полный метод
+    //0 - полный метод, 1 - метод выращивания
     double* x = new double [num_obs];
     double* y = new double [num_obs];
     char* type = new char[num_types];
@@ -230,7 +339,7 @@ int main()
             {
                 flag = 0;
                 random_opervar = rand() % (unar_oper+bin_oper);
-                success = tree[i].AddNode(type[0], random_oper, unar_oper, bin_oper, depth-1, &flag, 0);
+                success = tree[i].AddNode(type[0], random_opervar, unar_oper, bin_oper, depth-1, &flag, 0, switch_init);
             }
             success = 1;
             while (success == 1)
@@ -240,21 +349,25 @@ int main()
                 if(random_type == 1)
                 {
                     random_opervar = rand() % num_var;
-                    success = tree[i].AddNode(type[random_type], random_opervar, unar_oper, bin_oper, depth-1, &flag, 0);
+                    success = tree[i].AddNode(type[random_type], random_opervar, unar_oper, bin_oper, depth-1, &flag, 0, switch_init);
                 }
-                if(random_type == 2)
+                else if(random_type == 2)
                 {
-                    random_const = rand() % num_var;//тут узнать и поменять
-                    success = tree[i].AddNode(type[random_type], random_const, unar_oper, bin_oper, depth-1, &flag, 0);
+                    random_const = rand() % 11;//тут узнать и поменять
+                    success = tree[i].AddNode(type[random_type], random_const, unar_oper, bin_oper, depth-1, &flag, 0, switch_init);
                 }
             }
+            cout << "tree" << endl;
+            // читается дерево в глубину по правилу левый-корень-правый
+            tree[i].PrintTree();
         }
     }
-    cout << "tree" << endl;
-    // читается дерево в глубину по правилу левый-корень-правый
-    tree.PrintTree();
-    delete[] tree;
+
+
+
     delete[] x;
     delete[] y;
     delete[] type;
 }
+// нужно спросить как преобразовать дерево в математическое выражение, еще вот сейчас я использую две прототипные функции,
+// в которых написано одно и то же, но типы разные из-за типа констант, может можно по-другому сделать?
